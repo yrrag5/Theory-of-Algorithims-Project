927ddf68769ac674db81cb853948df67e3330c3c
yrrag5 <G00335806@gmit.ie>
2019-03-08 17:22:00 +0100

    Updating part 2 of SHA algorthim

--- SHA/SHA256.c
+++ SHA/SHA256.c
@@ -6,11 +6,41 @@
 
 void SHA256();
 
+// K constants, defined in Section 4.2.2.
+
+uint32_t K[] = {
+	 0x428a2f98 ,0x71374491 ,0xb5c0fbcf ,0xe9b5dba5
+    ,0x3956c25b ,0x59f111f1 ,0x923f82a4 ,0xab1c5ed5
+    ,0xd807aa98 ,0x12835b01 ,0x243185be ,0x550c7dc3
+    ,0x72be5d74 ,0x80deb1fe ,0x9bdc06a7 ,0xc19bf174
+    ,0xe49b69c1 ,0xefbe4786 ,0x0fc19dc6 ,0x240ca1cc
+    ,0x2de92c6f ,0x4a7484aa ,0x5cb0a9dc ,0x76f988da
+    ,0x983e5152 ,0xa831c66d ,0xb00327c8 ,0xbf597fc7
+    ,0xc6e00bf3 ,0xd5a79147 ,0x06ca6351 ,0x14292967
+    ,0x27b70a85 ,0x2e1b2138 ,0x4d2c6dfc ,0x53380d13
+    ,0x650a7354 ,0x766a0abb ,0x81c2c92e ,0x92722c85
+    ,0xa2bfe8a1 ,0xa81a664b ,0xc24b8b70 ,0xc76c51a3
+    ,0xd192e819 ,0xd6990624 ,0xf40e3585 ,0x106aa070
+    ,0x19a4c116 ,0x1e376c08 ,0x2748774c ,0x34b0bcb5 
+	,0x391c0cb3 ,0x4ed8aa4a ,0x5b9cca4f ,0x682e6ff3
+	,0x748f82ee ,0x78a5636f ,0x84c87814 ,0x8cc70208
+	,0x90befffa ,0xa4506ceb ,0xbef9a3f7 ,0xc67178f2
+};
+
+
 uint32_t sig0(uint32_t x);
 uint32_t sig1(uint32_t x);
 
 uint32_t rotr(uint32_t n, uint32_t x);
 uint32_t shr(uint32_t n, uint32_t x);
+
+uint32_t SIG0(uint32_t x);
+uint32_t SIG1(uint32_t x);
+
+uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
+
+
 
 
 int main(int argc, char *argv[]){
@@ -26,6 +56,7 @@ void SHA256(){
 	// Temp variables (Secion 6.2)
 	uint32_t T1, T2;
 	
+	// Intialising H constants
 	uint32_t H[8] = {
 		0x6a09e667,
 		0xbb67ae85,
@@ -35,46 +66,51 @@ void SHA256(){
 		0x9b05688c,
 		0x1f83d9ab,
 		0x5be0cd19
-	}
+	};
+	// Single message block
+	uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0, 0};
 	
-	uint32_t M[16];
+	int i, t;
 	
-	int t;
-	
-	for(t = 0; t < 16; t++){
-		T[t] = M[t];
-	}
-	
-	for (t - 16; t < 64; t++){
-		sig_l(W[t-2]) + W[t-7] + sig_0(W[t-15]) + W[t-16];
-	}		
-	
-	a = H[0]; b = H[1]; C = H[2]; d = H[3]; 
-	e = H[4]; f = H[5]; g = H[6]; h = H[7];
-	
-	for (t = 0; t < 64; t ++){
-		T1 = h + SIG_1[E] + Ch(e, f, g) + K[t] + W[t]
-		T2 = SIG_0(a) + Maj(a, b, c);
-		h=g;
-		g=f;
-		f=e;
-		e=d+T1;
-		d=c;
-		c=b;
-		b=a;
-		a=T1+T2;
-	}
-	
-	H[0] = a + H[0];
-	H[1] = b + H[1];
-	H[2] = c + H[2];
-	H[3] = d + H[3];
-	H[4] = e + H[4];
-	H[5] = f + H[5];
-	H[6] = g + H[6];
-	H[7] = h + H[7];
+	// Loops through message boxs
+	for int (int = 0; i < 1; i++){
+		
+		for(t = 0; t < 16; t++){
+			T[t] = M[t];
+		}
+		
+		for (t - 16; t < 64; t++){
+			W[t] = sigl(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+		}		
+		
+		a = H[0]; b = H[1]; C = H[2]; d = H[3]; 
+		e = H[4]; f = H[5]; g = H[6]; h = H[7];
+		
+		for (t = 0; t < 64; t ++){
+			T1 = h + SIG1[e] + Ch(e, f, g) + K[t] + W[t]
+			T2 = SIG0(a) + Maj(a, b, c);
+			h=g;
+			g=f;
+			f=e;
+			e=d+T1;
+			d=c;
+			c=b;
+			b=a;
+			a=T1+T2;
+		}
+		
+		// Step 4
+		H[0] = a + H[0];
+		H[1] = b + H[1];
+		H[2] = c + H[2];
+		H[3] = d + H[3];
+		H[4] = e + H[4];
+		H[5] = f + H[5];
+		H[6] = g + H[6];
+		H[7] = h + H[7];
+		}// i for
 
-	
+	print f("%x %x %x %x %x %x %x %x", H[0], H[2], H[3], H[4], H[5], H[6], H[7], H[8]);
 }
 
 uint32_t rotr(uint32_t n, uint32_t x){
@@ -86,10 +122,26 @@ uint32_t shr(uint32_t n, uint32_t x){
 }
 
 uint32_t sig0(uint32_t x){
-	return (rotr(7, x) ^ rote(18, x) ^ shr(3, x));	
+	return (rotr(7, x) ^ rotr(18, x) ^ shr(3, x));	
 }
 
 uint32_t sig1(uint32_t x){
-	return (rotr(17, x) ^ rote(19, x) ^ shr(10, x));	
+	return (rotr(17, x) ^ rotr(19, x) ^ shr(10, x));	
 
+}
+
+uint32_t SIG0(uint32_t x){
+	(rotr(2, x) ^ rotr(19,x) ^ rotr(22, x));
+}
+uint32_t SIG1(uint32_t x){
+	(rotr(6, x) ^ rotr(11,x) ^ rotr(2, x));
+
+}
+
+uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
+	return ()(x & y) ^ ((!x) & z));
+	
+}
+uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
+	return ()(x & y) ^ (x & z) ^ (y & z));
 }
