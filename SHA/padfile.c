01aa544d55bc89aef4b5de9c2f8f481f9ee39711
yrrag5 <G00335806@gmit.ie>
2019-03-25 19:51:45 +0000

    Updating padding

--- SHA/padfile.c
+++ SHA/padfile.c
@@ -4,28 +4,32 @@
 #include <stdint.h>
 
 union msgblock {
+	//8
     uint8_t  e[64];
+	//32
     uint32_t t[16];
+	//64
     uint64_t  s[8];
 };
 
-
+enum status {READ, PAD0, PAD1, FINISH}
 int main(int argc, char *argv[]) {
 
     union msgblock M;
-
-	
+		
     uint64_t nobits = 0;
 	// Message block 
     uint64_t nobytes;
 
-
+	enum status S = READ;
+	
     FILE* f;
     f = fopen(argv[1], "r");
 	
-	while (!feof(f)) {
+	int i;
+	
+	while (S == READ) {
 		nobytes = fread(M.e, 1, 64, f);
-		printf("Read %211u bytes\n", nobytes);
 		nobits = nobits + (nobytes * 8);
 		if (nobytes < 56) {
 			printf("I've found a block with less than 55 bytes.\n");
@@ -37,8 +41,30 @@ int main(int argc, char *argv[]) {
 				M.e[nobytes] = 0x00;
 			}
 			M.s[7] = nobits;
+			S = FINISH;
+		}
+		else if (nobytes < 64){
+			S = PAD0;
+			M.e[nobytes] = 0x80;
+			while (nobytes < 64) {
+				nobytes + 1;
+				M.e[nobytes] = 0x00;
+			}
+		}// else if
+		else if (feof(f)){
+			S = PAD1;
 		}
 	}//while
+	
+	if(S == PAD0 || S == PAD1){
+		for (i = 0; i < 56; i++){
+			M.e[i] = 0X00;
+			M.s[7] = nobits;
+		}
+	}// if
+	if (S == PAD1){
+		M.e[0] = 0x80;
+	}
 
 	fclose(f);
 	
